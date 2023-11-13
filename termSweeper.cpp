9a8b791dec112f909470e54da9bb251846a11d27
msharov <msharov2017@gmail.com>
2023-11-12 17:14:12 -0800

    modified the method for selecting a game

--- termSweeper.cpp
+++ termSweeper.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
 #include <limits>
 
-#define max_frame_side = 25
-#define max_bomb_saturation = 1.0
-#define min_bomb_saturation = 0.0
-
 using namespace std;
 
-void select_game_params()
-{
-	int board_width, board_height;
-	std::cout << "Please enter the desired width and height of the board" << std::endl;
-	std::cin >> board_width >> board_height;
+class GameBoard {
+	public:
+
+	int width, height, bombs;
+		//will have to make a board state or something like that?
+
+	void initialize_game_params()
+	{
+		int width_arr[3] = {9, 16, 16};
+		int height_arr[3] = {9, 16, 30};
+		int bomb_arr[3] = {10, 40, 99};
+
+		int game_select;
+
+
+		cout << "Welcome to termSweeper!" << endl;
+		cout << "Please select a game difficulty: \n" << 
+				"(0) Easy, 9x9 board, 10 mines \n" << 
+				"(1) Medium 16x16 board, 40 mines \n" << 
+				"(2) Hard 16x30, 99 mines" << endl;
 	
-	while (1) {
-		if (cin.fail()) {
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Please enter an integer value for height and width" << std::endl;
-			std::cin >> board_width >> board_height;
+		cin >> game_select; 
+
+		while (1) {
+			if (cin.fail() || game_select > 2 || game_select < 0) {
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Please enter a number (0-2):" << endl;
+				cin >> game_select;
+			}
+			else {
+				width = width_arr[game_select];
+				height = height_arr[game_select];
+				bombs = bomb_arr[game_select];
+				break;
+			}		
 		}
-		if (board_width > 25 || board_height >> 25 ) {
-			std::cin.clear();
-			std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			std::cout << "Sorry, maximum side size is 25, try again!" << std::endl;
-			std::cin >>  board_width >> board_height;
-		}
-		if (!cin.fail()) {
-			break;
-		}		
 	}
 
-	std::cout << "You have selected: width = " << board_width << " and height = " << board_height << std::endl;
-}
+	void display_board()
+	{
+			
+	}
+};
 
 
 
-
-int main(int argc, char *argv[])
-{
-	select_game_params();
+int main(int argc, char *argv[]){
+	
+	GameBoard board;
+	board.initialize_game_params();
+	cout << board.width << board.height << board.bombs << endl;	
 	return 0;
 }
